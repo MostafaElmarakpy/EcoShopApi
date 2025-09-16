79bd1a5e98643853605b219126b5ee371244e5f9
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-17 02:40:33 +0300

    Add DocumentSettings class for file upload and deletion
    Introduces the `DocumentSettings` class in the `EcoShopApi.Helper` namespace.
    This class includes two static methods: `UploadFile`, which uploads files with a unique name,
    and `DeleteFile`, which removes specified files from a designated folder after checking for their existence.

---
+++ EcoShopApi/Helper/DocumentSettings.cs
@@ -0,0 +1,31 @@
+﻿namespace EcoShopApi.Helper
+{
+    public class DocumentSettings
+    {
+        public static string UploadFile(IFormFile file, string folderName)
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", folderName);
+            string fileName = $"{Guid.NewGuid()} {file.FileName}";
+            string filePath = Path.Combine(folderPath, fileName);
+
+            using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+            //using var sr = new StreamReader(fs);
+            //string content = sr.ReadToEnd();
+            file.CopyTo(fs);
+            return fileName;
+        }
+        public static void DeleteFile(string fileName, string folderName)
+
+        {
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\files", folderName, fileName);
+
+            if (File.Exists(folderPath))
+            {
+                File.Delete(folderPath);
+            }
+
+        }
+
+    }
+}
