dc42400e2314743108b987d315120dca52f8b7ab
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-17 02:55:32 +0300

    create Product Repository

---
+++ EcoShopApi.Infrastructure/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using EcoShopApi.Application.Common.Interfaces;
+using EcoShopApi.Domain.Entities;
+using EcoShopApi.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoShopApi.Infrastructure.Repository
+{
+    public class ProductRepository : GenaricRepository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Product entity)
+        {
+            _db.Products.Update(entity);
+        }
+
+    }
+}
