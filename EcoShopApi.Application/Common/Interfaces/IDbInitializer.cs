6a4f081bd29f0652f5db6354366a62bed7cd737b
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-19 22:09:00 +0300

    Update Data Seed

---
+++ EcoShopApi.Application/Common/Interfaces/IDbInitializer.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcoShopApi.Application.Common.Interfaces
+{
+    public interface IDbInitializer
+    {
+        void Initialize();
+    }
+}
