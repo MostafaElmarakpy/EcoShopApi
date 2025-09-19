619b345f89f7f305ef5becbff556807ca85feb8c
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-19 22:07:37 +0300

    Create Utility SD

---
+++ EcoShopApi.Application/Common/Utility/SD.cs
@@ -0,0 +1,11 @@
+namespace EcoShopApi.Application.Common.Utility
+{
+    public static class SD
+    {
+
+        public const string Role_Admin = "Admin";
+        public const string Role_User = "User";
+
+
+    }
+}
